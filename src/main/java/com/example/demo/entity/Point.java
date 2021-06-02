ad05df5465dd954ca584af78146080097fcafe0a
Ivan Olhovoy <akim.rabinko@easypark.net>
2021-06-02 13:35:07 +0300

    refactor findById() in PlaceService

--- src/main/java/com/example/demo/entity/Point.java
+++ src/main/java/com/example/demo/entity/Point.java
@@ -1,9 +1,19 @@
 package com.example.demo.entity;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Value;
 
 @Value
 public class Point {
     double lat;
     double lon;
+
+    @JsonCreator
+    public Point(
+            @JsonProperty("lat") double lat,
+            @JsonProperty("lon") double lon) {
+        this.lat = lat;
+        this.lon = lon;
+    }
 }
